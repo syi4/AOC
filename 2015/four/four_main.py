e12c8b9fa7def30c645c6b2e2e8f998a81b04972
syi4 <72623292+yisy87@users.noreply.github.com>
2024-12-01 21:49:05 -0500

    2015 day four part one

---
+++ 2015/four/four_main.py
@@ -0,0 +1,17 @@
+import hashlib
+
+def find_min_number(input="iwrupvqb", target_zeros=5):
+     number = 0
+     prefix = '0' * target_zeros
+    
+     while True:
+        hash_input = f"{input}{number}".encode('utf-8')
+        hash_result = hashlib.md5(hash_input).hexdigest()
+        
+        if hash_result.startswith(prefix):
+            return number  
+        
+        number += 1 
+
+print(find_min_number())
+
