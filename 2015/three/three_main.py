b2413a61e57a60be55f2021521ed7ed70c4e4520
syi4 <72623292+yisy87@users.noreply.github.com>
2024-12-01 20:43:56 -0500

    2015 day three part two

--- 2015/three/three_main.py
+++ 2015/three/three_main.py
@@ -19,7 +19,31 @@ def get_unique_houses(input):
 
     return len(house_set)
 
-print(get_unique_houses(data))
+def get_unique_by_santa_and_robo(input):
+    santa_current = (0, 0)
+    robo_current = (0, 0)
+    house_set = {santa_current}
+
+    moves = {
+        '^': (0, 1),  
+        'v': (0, -1),  
+        '<': (-1, 0),  
+        '>': (1, 0)    
+    }
+
+    for i, dir in enumerate(input):
+        dx, dy = moves[dir]
+        
+        if i % 2 == 0:
+            santa_current = (santa_current[0] + dx, santa_current[1] + dy)
+            house_set.add(santa_current)
+        else:
+            robo_current = (robo_current[0] + dx, robo_current[1] + dy)
+            house_set.add(robo_current)
+    
+    return len(house_set)
+
+print(get_unique_by_santa_and_robo(data))
 
 
 
