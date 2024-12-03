1d6c396734decb284334af853d9fbc84f51973cb
syi4 <72623292+yisy87@users.noreply.github.com>
2024-12-03 01:38:46 -0500

    2024 day two part two

--- 2024/two/two_main.py
+++ 2024/two/two_main.py
@@ -2,19 +2,30 @@ with open('two_input.txt', 'r') as content:
     lines = content.readlines()
     data = [list(map(int, line.split())) for line in lines]
 
-def get_safe(input):
+def is_safe(level):
+    is_asc = all(level[i] <= level[i+1] and 1 <= (level[i+1] - level[i]) <= 3 for i in range(len(level) - 1))
+    is_desc = all(level[i] >= level[i + 1] and 1 <= (level[i] - level[i + 1]) <= 3 for i in range(len(level) - 1))
+
+    return is_asc or is_desc
+
+def get_safe_two(input):
     safe = 0
 
     for level in input:
-        is_asc = all(level[i] <= level[i+1] and 1 <= (level[i+1] - level[i]) <= 3 for i in range(len(level) - 1))
-        is_desc = all(level[i] >= level[i + 1] and 1 <= (level[i] - level[i + 1]) <= 3 for i in range(len(level) - 1))
-
-        if is_asc or is_desc:
+        if is_safe(level):
             safe += 1
+        else:
+            for i in range(len(level)):
+                new_level = level[:i] + level[i + 1:]
+
+                if is_safe(new_level):
+                    safe += 1
+                    break
 
     return safe
 
-print(get_safe(data))
+
+print(get_safe_two(data))
 
 
 
